4b83df9c2b00eae48d88e189731e2483ea4f3fc1
wc <chong.wu.xiangfan@gmail.com>
2023-05-05 06:44:42 +0800

    fix

--- src/features/audio/audio.service.ts
+++ src/features/audio/audio.service.ts
@@ -6,6 +6,14 @@ import moment from 'moment';
 import WebSocket from 'ws';
 import Speaker from 'speaker';
 import {Readable} from 'stream'
+import { Server } from 'socket.io';
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  WsResponse,
+} from '@nestjs/websockets';
 
 type Text2SpeechResponse = {
   "code": number,
@@ -20,7 +28,14 @@ type Text2SpeechResponse = {
 
 
 @Injectable()
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
 export class AudioService {
+  @WebSocketServer()
+  server: Server;
   private readonly appId = this.configService.get('kdxf.appId');
   private readonly apiSecret = this.configService.get('kdxf.apiSecret');
   private readonly apiKey = this.configService.get('kdxf.apiKey');
@@ -105,7 +120,7 @@ export class AudioService {
 
     this.text2speechClient.onopen = () => {
       this.logger.debug('ws connected');
-      this.text2speech('hello world');
+      // this.text2speech('hello world');
     };
   }
 
@@ -138,7 +153,9 @@ export class AudioService {
     return authorization;
   }
 
+  @SubscribeMessage('events')
   text2speech(
+    @MessageBody() data: {text: string},
     text: string,
     speed = 50,
     volume = 50,
@@ -159,7 +176,7 @@ export class AudioService {
       },
       data: {
         status: 2,
-        text: Buffer.from(text).toString('base64'),
+        text: Buffer.from(data.text).toString('base64'),
       },
     };
     this.text2speechClient.send(JSON.stringify(payload));
