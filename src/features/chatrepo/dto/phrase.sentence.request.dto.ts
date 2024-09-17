15f1ff17dc585257fc129bed6fc26f10bdde89cb
api <project_61713533_bot_7b7493215f04935978141ff79a4b42b8@noreply.gitlab.com>
2024-09-17 01:33:01 +0000

    commit

--- src/features/chatrepo/dto/phrase.sentence.request.dto.ts
+++ src/features/chatrepo/dto/phrase.sentence.request.dto.ts
@@ -6,151 +6,207 @@ import { IsNotEmpty } from 'class-validator'
 export class CreatePhraseSentenceRequestDTO {
   @codeGen('11041')
   @ApiProperty({
-    description: '',
+    description: '短语',
     required: false,
   })
   phrase?: string
 
   @codeGen('11042')
   @ApiProperty({
-    description: '',
+    description: '句子',
     required: false,
   })
   sentence?: string
 
   @codeGen('11043')
   @ApiProperty({
-    description: '',
+    description: '音色',
     required: false,
   })
   voice?: string
 
   @codeGen('11044')
   @ApiProperty({
-    description: '',
+    description: '音频',
     required: false,
   })
   audio?: string
 
   @codeGen('11045')
   @ApiProperty({
-    description: '',
+    description: '语速',
     required: false,
   })
   @Transform(getTransformer('numberTransformer'))
   speed?: number
+
+  @codeGen('11046')
+  @ApiProperty({
+    description: '单元',
+    required: false,
+  })
+  unit?: string
+
+  @codeGen('11047')
+  @ApiProperty({
+    description: '年级',
+    required: false,
+  })
+  grade?: string
 }
 
 export class UpdatePhraseSentenceRequestDTO {
   @codeGen('11041')
   @ApiProperty({
-    description: '',
+    description: '短语',
     required: false,
   })
   phrase?: string
 
   @codeGen('11042')
   @ApiProperty({
-    description: '',
+    description: '句子',
     required: false,
   })
   sentence?: string
 
   @codeGen('11043')
   @ApiProperty({
-    description: '',
+    description: '音色',
     required: false,
   })
   voice?: string
 
   @codeGen('11044')
   @ApiProperty({
-    description: '',
+    description: '音频',
     required: false,
   })
   audio?: string
 
   @codeGen('11045')
   @ApiProperty({
-    description: '',
+    description: '语速',
     required: false,
   })
   @Transform(getTransformer('numberTransformer'))
   speed?: number
+
+  @codeGen('11046')
+  @ApiProperty({
+    description: '单元',
+    required: false,
+  })
+  unit?: string
+
+  @codeGen('11047')
+  @ApiProperty({
+    description: '年级',
+    required: false,
+  })
+  grade?: string
 }
 
 export class FindOnePhraseSentenceRequestDTO {
   @codeGen('11041')
   @ApiProperty({
-    description: '',
+    description: '短语',
     required: false,
   })
   phrase?: string
 
   @codeGen('11042')
   @ApiProperty({
-    description: '',
+    description: '句子',
     required: false,
   })
   sentence?: string
 
   @codeGen('11043')
   @ApiProperty({
-    description: '',
+    description: '音色',
     required: false,
   })
   voice?: string
 
   @codeGen('11044')
   @ApiProperty({
-    description: '',
+    description: '音频',
     required: false,
   })
   audio?: string
 
   @codeGen('11045')
   @ApiProperty({
-    description: '',
+    description: '语速',
     required: false,
   })
   @Transform(getTransformer('numberTransformer'))
   speed?: number
+
+  @codeGen('11046')
+  @ApiProperty({
+    description: '单元',
+    required: false,
+  })
+  unit?: string
+
+  @codeGen('11047')
+  @ApiProperty({
+    description: '年级',
+    required: false,
+  })
+  grade?: string
 }
 
 export class FindAllPhraseSentenceRequestDTO extends PagingRequestDTO {
   @codeGen('11041')
   @ApiProperty({
-    description: '',
+    description: '短语',
     required: false,
   })
   phrase?: string
 
   @codeGen('11042')
   @ApiProperty({
-    description: '',
+    description: '句子',
     required: false,
   })
   sentence?: string
 
   @codeGen('11043')
   @ApiProperty({
-    description: '',
+    description: '音色',
     required: false,
   })
   voice?: string
 
   @codeGen('11044')
   @ApiProperty({
-    description: '',
+    description: '音频',
     required: false,
   })
   audio?: string
 
   @codeGen('11045')
   @ApiProperty({
-    description: '',
+    description: '语速',
     required: false,
   })
   @Transform(getTransformer('numberTransformer'))
   speed?: number
+
+  @codeGen('11046')
+  @ApiProperty({
+    description: '单元',
+    required: false,
+  })
+  unit?: string
+
+  @codeGen('11047')
+  @ApiProperty({
+    description: '年级',
+    required: false,
+  })
+  grade?: string
 }
--- src/features/chatrepo/entities/phrase.sentence.entity.ts
+++ src/features/chatrepo/entities/phrase.sentence.entity.ts
@@ -53,4 +53,20 @@ export class PhraseSentence extends BaseModel<PhraseSentence> {
   })
   // value from `0.25` to `4.0`. `1.0` is the default.
   speed: number
+
+  @Column({
+    allowNull: true,
+    type: DataType.STRING(40),
+    comment: '单元',
+  })
+  @codeGen('11046')
+  unit?: string
+
+  @Column({
+    allowNull: true,
+    type: DataType.STRING(40),
+    comment: '年级',
+  })
+  @codeGen('11047')
+  grade?: string
 }
